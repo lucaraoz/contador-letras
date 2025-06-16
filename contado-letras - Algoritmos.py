d4a86366681ee7ddc542d0913f6c80bad3a0231c
Luca <102250346+lucaraoz@users.noreply.github.com>
2025-06-16 11:30:00 -0300

    Contador de letras de una palabra en base a una práctica de algoritmos. Aplicación de pseudocódigo y diagrama de flujo.

---
+++ contado-letras - Algoritmos.py
@@ -0,0 +1,16 @@
+#----------------------------------#
+#------------ALGORITMOS------------#
+#-------------PRACTICA-------------#
+
+#Sentencia = problema sencillo
+#ALGORITMOS: serie de pasos secuneciales para poder resolver una problemática de forma eficiente.
+
+#Paso 1: pedir al usuario que ingrese una palabra para poder contar sus letras
+palabra_ingresada = input("Ingresá una palabra para contar sus letras: ")
+
+#Paso 2: contar la cantidad de letras
+cantidad_letras = len(palabra_ingresada)
+
+#Paso 3: mostrar al usuario la cantidad de letras.
+print("La palabra ingresada tiene", cantidad_letras, "letras.")
+
